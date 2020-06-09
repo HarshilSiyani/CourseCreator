0f44a0d2017a95bd8860d16a02b674a23b899e31
harshilsiyani <siyaniharsh@gmail.com>
2020-06-09 11:53:30 +1000

    ARCH: skeleton updated with gem files

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :courses, except: [:edit, :index, :destroy, :update] do
+    resources :enrollment, only: :create
+    resources :module, only: [:new, :create]
+  end
+
+  patch '/courses/:id/publish', to: 'courses#publish'
+
 end
