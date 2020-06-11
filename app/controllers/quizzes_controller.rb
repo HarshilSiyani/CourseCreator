9d93cd6af163f79852ce7ddaa544e39487660c7f
Erik Tran <erik.tran.tt@gmail.com>
2020-06-11 21:01:00 +1000

    UPDATED: using strong params & ActionController::NestedAttributes methods to handle create, update and destroy questions. Optimized code.

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -7,10 +7,8 @@ class QuizzesController < ApplicationController
   end
 
   def create
-    @quiz = Quiz.new(quiz_params)
+    @quiz = Quiz.new(quiz_create_params)
     @quiz.study_module.course = @course
-
-    create_questions(@quiz)
 
     if @quiz.save
       redirect_to edit_course_path(@course, study_module_id: @quiz.study_module.id)
@@ -21,9 +19,11 @@ class QuizzesController < ApplicationController
 
   def update
     @quiz = Quiz.find(params[:id])
+    @quiz.update quiz_params
+    @quiz.update question_params
 
-    handle_questions(@quiz)
-    # raise
+    # handle_questions(@quiz) # OLD CODE
+
     redirect_to edit_course_path(@course, study_module_id: @quiz.study_module.id)
   end
 
@@ -40,62 +40,73 @@ class QuizzesController < ApplicationController
   def quiz_params
     params
       .require(:quiz)
-      .permit(:text, study_module_attributes: StudyModule.attribute_names.map(&:to_sym).push(:_destroy))
+      .permit(:text, study_module_attributes: StudyModule.attribute_names.map(&:to_sym))
   end
 
   def question_params
+    tmp_params = params
+                 .require(:quiz)
+                 .permit(questions_attributes: Question.attribute_names.map(&:to_sym).push(:_destroy))
+
+    params.permit(:id).merge!(tmp_params)
+  end
+
+  def quiz_create_params
     params
       .require(:quiz)
-      .permit(questions_attributes: Question.attribute_names.map(&:to_sym).push(:_destroy))
+      .permit(
+        :text,
+        study_module_attributes: StudyModule.attribute_names.map(&:to_sym),
+        questions_attributes: Question.attribute_names.map(&:to_sym)
+      )
   end
 
-  def handle_questions(quiz)
-    # HANDLE QUESTIONS ACTIONS: CREATE, UPDATE, DELETE
-    destroy_questions(quiz)
+  # def handle_questions(quiz)
+  #   # HANDLE QUESTIONS ACTIONS: CREATE, UPDATE, DELETE
+  #   destroy_questions(quiz)
 
-    create_questions(quiz)
-    quiz.save
+  #   quiz.save if create_questions(quiz)
 
-    update_questions(quiz)
-    quiz.save
-  end
+  #   quiz.save if update_questions(quiz)
+  # end
 
-  def create_questions(quiz)
-    createables = question_params["questions_attributes"]
-                  .values.select { |question| question["id"].nil? }
-                  .map do |question|
-                    { text: question["text"] }
-                  end
-    return if createables.nil?
+  # def create_questions(quiz)
+  #   createables = question_params["questions_attributes"]
+  #                 .values.select { |question| question["id"].nil? }
+  #                 .map do |question|
+  #                   { text: question["text"] }
+  #                 end
+  #   return false if createables.nil?
 
-    quiz.questions.build(createables)
-  end
+  #   quiz.questions.build(createables)
+  #   return true
+  # end
 
-  def update_questions(_quiz)
-    updateables = question_params["questions_attributes"]
-                  .values.reject { |question| question["_destroy"] == "1" || question["id"].nil? }
-    updateable_text = updateables.map { |question| { text: question["text"] } }
-    updateable_ids = updateables.map { |question| question["id"].to_i }
+  # def update_questions(_quiz)
+  #   updateables = question_params["questions_attributes"]
+  #                 .values.reject { |question| question["_destroy"] == "1" || question["id"].nil? }
+  #   updateable_text = updateables.map { |question| { text: question["text"] } }
+  #   updateable_ids = updateables.map { |question| question["id"].to_i }
 
-    current_questions = Question.where(id: updateable_ids).map(&:text)
-    # current_questions == updateables.map { |question| question["text"] }
-    # => TRUE if no changes, FALSE if there are changes
-    return if updateable_ids.nil? || current_questions == updateables.map { |question| question["text"] }
+  #   current_questions = Question.where(id: updateable_ids).map(&:text)
+  #   # current_questions == updateables.map { |question| question["text"] }
+  #   # => TRUE if no changes, FALSE if there are changes
+  #   return false if updateable_ids.nil? || current_questions == updateables.map { |question| question["text"] }
 
-    Question.update(updateable_ids, updateable_text)
-  end
+  #   Question.update(updateable_ids, updateable_text)
+  #   return true
+  # end
 
-  # Question.find_or_initialize_by(question_params["questions_attributes"].values)
-  def destroy_questions(quiz)
-    destroyables = question_params["questions_attributes"]
-                  .values.select { |question| question["_destroy"] == "1" }
-    destroyable_ids = destroyables.map { |question| question["id"].to_i }
+  # # Question.find_or_initialize_by(question_params["questions_attributes"].values)
+  # def destroy_questions(quiz)
+  #   destroyables = question_params["questions_attributes"]
+  #                 .values.select { |question| question["_destroy"] == "1" }
+  #   destroyable_ids = destroyables.map { |question| question["id"].to_i }
 
-    return if destroyable_ids.nil?
+  #   return if destroyable_ids.nil?
 
-    destroyable_ids&.each do |id|
-      quiz.questions.destroy(Question.find(id))
-    end
-  end
-
+  #   destroyable_ids&.each do |id|
+  #     quiz.questions.destroy(Question.find(id))
+  #   end
+  # end
 end
