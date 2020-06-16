1fe9775ffea9f77442d68198f21950350230dad6
harshilsiyani <siyaniharsh@gmail.com>
2020-06-16 11:07:54 +1000

    Modal updated

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -45,7 +45,7 @@ tl.from(hero,1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut})
 .fromTo(hero, 1.2, {width: "100%"}, {width: "80%", ease: Power2.easeInOut})
 .fromTo(slider, 1.2, {x: "-100%"}, {x: "0%", ease: Power2.easeInOut}, "-=1.2");
 
-
+new ClipboardJS('.copy');
 
 
 
