a1a57847e6d241c4de03f8ab0204f621e1452e58
harshilsiyani <siyaniharsh@gmail.com>
2020-06-17 20:54:30 +1000

    Removed TweenMax

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -58,20 +58,4 @@ document.addEventListener('turbolinks:load', () => {
 });
 
 
-const hero = document.querySelector(".hero");
-const slider = document.querySelector(".slider");
-
-
-const tl = new TimelineMax();
-
-tl.from(hero,1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut})
-.fromTo(hero, 1.2, {width: "100%"}, {width: "80%", ease: Power2.easeInOut})
-.fromTo(slider, 1.2, {x: "-100%"}, {x: "0%", ease: Power2.easeInOut}, "-=1.2");
-
-
-
-
-
-
-
 
