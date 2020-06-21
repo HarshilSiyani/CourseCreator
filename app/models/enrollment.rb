f453145bd636ff24df09254b4fb5a83b648a062a
Erik Tran <erik.tran.tt@gmail.com>
2020-06-21 12:02:50 +1000

    refactor(wip): added additional instance methods for validation and navigation

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -2,4 +2,9 @@ class Enrollment < ApplicationRecord
   belongs_to :course
   belongs_to :student, class_name: "User"
   # belongs_to :user, foreign_key: "student_id"
+
+  def next_module_index(current_module)
+    self.module_index = current_module.index if current_module.index > module_index
+    save
+  end
 end
--- app/models/study_module.rb
+++ app/models/study_module.rb
@@ -7,26 +7,47 @@ class StudyModule < ApplicationRecord
   before_create :set_next_id
 
   def next_module
-    self.class.where(course_id: course_id).where("id > ?", id).first
+    # self.class.where(course_id: course_id).where("id > ?", id).first
+    course.study_modules.where("id > ?", id).includes(:contentable).first
   end
 
   def previous_module
-    self.class.where(course_id: course_id).where("id < ?", id).last
+    # self.class.where(course_id: course_id).where("id < ?", id).last
+    course.study_modules.where("id < ?", id).includes(:contentable).last
   end
 
   def first_module
-    self.class.where(course_id: course_id).first
+    # self.class.where(course_id: course_id).first
+    course.study_modules.includes(:contentable).first
   end
 
   def last_module
-    self.class.where(course_id: course_id).last
+    # self.class.where(course_id: course_id).last
+    course.study_modules.includes(:contentable).last
+  end
+
+  def first_module?
+    self == first_module
+  end
+
+  def last_module?
+    self == last_module
+  end
+
+  def lesson?
+    contentable_type == Lesson.to_s
+  end
+
+  def quiz?
+    contentable_type == Quiz.to_s
   end
 
   private
 
   def set_next_id
-    if course.last_module
-      self.index = course.last_module.index + 1
+    last_module = course.last_module
+    if last_module
+      self.index = last_module.index + 1
     else
       self.index = 1
     end
