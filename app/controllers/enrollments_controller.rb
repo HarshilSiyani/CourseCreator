65672af250a7421ae35f9665d4eddd2e32691e48
Erik Tran <erik.tran.tt@gmail.com>
2020-06-21 14:23:54 +1000

    refactor: apply model instances' methods to reduce code complexity

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -8,12 +8,9 @@ class EnrollmentsController < ApplicationController
     @enrollment = Enrollment.new(enrollment_params)
     @enrollment.student = current_user
     @enrollment.module_index = 1
+
     if @enrollment.save
-      flash[:notice] = render_to_string(partial: "chat/messages/welcome", locals: { user: current_user })
-      NotificationChannel.broadcast_to(
-        @enrollment.course.teacher,
-        render_to_string(partial: "chat/messages/enrollment", locals: { user: current_user })
-      )
+      notifiy_new_enrollment(@enrollment)
       redirect_to user_enrollments_path(current_user)
     else
       redirect_to course_path(@enrollment.course)
@@ -30,7 +27,11 @@ class EnrollmentsController < ApplicationController
     params.permit(:course_id)
   end
 
-  # def enrollment_set
-  #   @enrollment = Enrollment.find(params[:id])
-  # end
+  def notifiy_new_enrollment(enrollment)
+    flash[:notice] = render_to_string(partial: "chat/messages/welcome", locals: { user: current_user })
+    NotificationChannel.broadcast_to(
+      enrollment.course.teacher,
+      render_to_string(partial: "chat/messages/enrollment", locals: { user: current_user })
+    )
+  end
 end
--- app/controllers/study/courses_controller.rb
+++ app/controllers/study/courses_controller.rb
@@ -1,40 +1,40 @@
-module Study
-  class CoursesController < ApplicationController
-    layout "student_view"
-    def show
-      @course = Course.find(params[:id])
-      redirect_to course_path(@course) and return unless current_user_enrolled?(@course)
+class Study::CoursesController < ApplicationController
+  layout "student_view"
+  def show
+    @course = Course.find(params[:id])
+    redirect_to course_path(@course) and return unless current_user.currently_enrolled?(@course)
 
-      if params[:study_module_id]
-        @current_module = @course.study_modules.find(params[:study_module_id])
-      else
-        @current_module = @course.study_modules.first
-      end
+    # @current_module = set_current_module(@course, params[:study_module_id])
+    @current_module = @course.find_module_or_first(params[:study_module_id])
+    @contentable = @current_module.contentable
+    redirect_to study_course_path(@course, study_module_id: @current_module) and return unless params[:study_module_id]
 
-      @contentable = @current_module.contentable
-      @enrollment = current_user.enrollments.find_by(course_id: @course.id)
-      @enrollment.module_index = @current_module.index if @current_module.index > @enrollment.module_index
-      @enrollment.save
-      # @quiz_passed control how the next button to go to next study module
-      # if @current_module is Lesson then able to go next to module
-      # if @current_module is a Quiz and student already passed this quiz
-      @quiz_passed = @current_module.contentable_type == Lesson.to_s || (@current_module.contentable_type == Quiz.to_s && @enrollment.module_index > @current_module.index)
+    @enrollment = current_user.find_my_enrollment(@course)
+    @enrollment.next_module_index(@current_module)
+    # @quiz_passed control how the next button to go to next study module
+    # if @current_module is Lesson then able to go next to module
+    # if @current_module is a Quiz and student already passed this quiz
+    @quiz_passed = @current_module.lesson? ||
+                    (@current_module.quiz? && @enrollment.module_index > @current_module.index)
+  end
+
+  def current_progress
+    @course = Course.find(params[:id])
+    enrollment = current_user.enrollments.find_by(course: @course)
+
+    respond_to do |format|
+      format.json { render head :no_content }
+      format.json { render json: { current_module_index: enrollment.module_index } }
     end
+  end
 
-    def current_progress
-      @course = Course.find(params[:id])
-      enrollment = current_user.enrollments.find_by(course: @course)
+  private
 
-      respond_to do |format|
-        format.json { render head :no_content }
-        format.json { render json: { current_module_index: enrollment.module_index } }
-      end
-    end
-
-    private
-
-    def current_user_enrolled?(course)
-      current_user.enrollments.map(&:course).include?(course)
+  def set_current_module(course, module_id = nil)
+    if module_id
+      course.find_module(module_id)
+    else
+      course.first_module
     end
   end
 end
