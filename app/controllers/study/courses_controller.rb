1eb9db1d634f19f5040fcc8d32d0430d70067864
Erik Tran <erik.tran.tt@gmail.com>
2020-06-21 14:25:18 +1000

    refactor: remove duplicated method

--- app/controllers/study/courses_controller.rb
+++ app/controllers/study/courses_controller.rb
@@ -4,7 +4,6 @@ class Study::CoursesController < ApplicationController
     @course = Course.find(params[:id])
     redirect_to course_path(@course) and return unless current_user.currently_enrolled?(@course)
 
-    # @current_module = set_current_module(@course, params[:study_module_id])
     @current_module = @course.find_module_or_first(params[:study_module_id])
     @contentable = @current_module.contentable
     redirect_to study_course_path(@course, study_module_id: @current_module) and return unless params[:study_module_id]
@@ -25,16 +24,6 @@ class Study::CoursesController < ApplicationController
     respond_to do |format|
       format.json { render head :no_content }
       format.json { render json: { current_module_index: enrollment.module_index } }
-    end
-  end
-
-  private
-
-  def set_current_module(course, module_id = nil)
-    if module_id
-      course.find_module(module_id)
-    else
-      course.first_module
     end
   end
 end
